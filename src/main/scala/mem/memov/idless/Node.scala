e04a5c61f88721d11f24dc05b5de730f1261d8f9
Alexander Novikov <anovikov@tsum.ru>
2022-01-05 14:15:44 +0300

    using paths

--- src/main/scala/mem/memov/idless/Node.scala
+++ src/main/scala/mem/memov/idless/Node.scala
@@ -5,13 +5,15 @@ class Node private (sourceBlock: => Vector[Node], targetBlock: => Vector[Node]):
   lazy val sources: Vector[Node] = sourceBlock
   lazy val targets: Vector[Node] = targetBlock
 
-  def withSource(node: Node): Node =
-    if sources.length <= node.targets.length then
-      lazy val modifiedTarget: Node = new Node(sources :+ newSource, targets)
-      lazy val newSource: Node = new Node(node.sources, node.targets :+ modifiedTarget)
-      modifiedTarget
-    else
-      node.withTarget(this).targets.last
+  def withTarget(): Node =
+    lazy val modifiedSource: Node = new Node(sources, targets :+ newTarget)
+    lazy val newTarget: Node = new Node(Vector(modifiedSource), Vector.empty)
+    modifiedSource
+
+  def withSource(): Node =
+    lazy val modifiedTarget: Node = new Node(sources :+ newSource, targets)
+    lazy val newSource: Node = new Node(Vector.empty, Vector(modifiedTarget))
+    modifiedTarget
 
   def withTarget(node: Node): Node =
     if targets.length <= node.sources.length then
@@ -20,6 +22,29 @@ class Node private (sourceBlock: => Vector[Node], targetBlock: => Vector[Node]):
       modifiedSource
     else
       node.withSource(this).sources.last
+
+  def withSource(node: Node): Node =
+    if sources.length <= node.targets.length then
+      lazy val modifiedTarget: Node = new Node(sources :+ newSource, targets)
+      lazy val newSource: Node = new Node(node.sources, node.targets :+ modifiedTarget)
+      modifiedTarget
+    else
+      node.withTarget(this).targets.last
+
+  def withTarget(path: Vector[(Boolean, Int)]): Node =
+    val node = this.turn(path)
+    this.withTarget(node)
+
+  def withSource(path: Vector[(Boolean, Int)]): Node =
+    val node = this.turn(path)
+    this.withSource(node)
+
+  def turn(path: Vector[(Boolean, Int)]): Node =
+    path.foldLeft(this)(
+      (node, step) => step match
+        case (true, index) => node.targets(index)
+        case (false, index) => node.sources(index)
+    )
 
 object Node:
   def empty =
