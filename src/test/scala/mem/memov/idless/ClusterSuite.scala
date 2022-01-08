6e85f8c2822bbb8b4d2ecc0bccdc84bdc6128bb7
Alexander Novikov <anovikov@tsum.ru>
2022-01-08 16:01:25 +0300

    building a graph from root node navigating with paths

--- src/test/scala/mem/memov/idless/ClusterSuite.scala
+++ src/test/scala/mem/memov/idless/ClusterSuite.scala
@@ -34,3 +34,19 @@ class ClusterSuite  extends AnyFunSuite:
     assert(a.beat(Vector(Step(0, 1))) == b)
     assert(b.beat(Vector(Step(1, 0))) == a)
   }
+
+  test("It creates a graph staying rooted in the first vertex") {
+    val g = Cluster.empty
+
+    g
+      .beat(
+        Vector(Step(0, 5), Step(0, 6))
+      )
+      .link(
+        0,
+        0,
+        g.beat(Vector(Step(1, 12), Step(1, 14)))
+      )
+
+    assert(g.beat(Vector(Step(0, 5), Step(0, 6), Step(0, 0))) == g.beat(Vector(Step(1, 12), Step(1, 14), Step(0, 0))))
+  }
